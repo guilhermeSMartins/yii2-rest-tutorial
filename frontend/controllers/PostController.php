82347406cdd85ad68bde53f7180aa70a7bf6b284
guilhermeSmartins <mineg4500@gmail.com>
2021-10-24 18:38:22 -0300

    check for auth made

--- frontend/controllers/PostController.php
+++ frontend/controllers/PostController.php
@@ -2,9 +2,12 @@
 
 namespace frontend\controllers;
 
+use Yii;
+
 use frontend\resource\Post;
 use yii\filters\auth\HttpBearerAuth;
 use yii\rest\ActiveController;
+use yii\web\ForbiddenHttpException;
 
 class PostController extends ActiveController
 {
@@ -20,5 +23,12 @@ class PostController extends ActiveController
         ];
 
         return $behaviors;
+    }
+
+    public function checkAccess($action, $model = null, $params = [])
+    {
+        if(in_array($action, ['update', 'delete']) && $model->created_by !== Yii::$app->user->id) {
+            throw new ForbiddenHttpException('no permission for these methods');
+        }
     }
 }

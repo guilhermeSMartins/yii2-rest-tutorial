96991795ce8c7ccc2b89dd73ff40fd063421208a
guilhermeSmartins <mineg4500@gmail.com>
2021-10-24 18:23:10 -0300

    behaviors made

--- frontend/controllers/PostController.php
+++ frontend/controllers/PostController.php
@@ -2,10 +2,23 @@
 
 namespace frontend\controllers;
 
-use common\models\Post;
+use frontend\resource\Post;
+use yii\filters\auth\HttpBearerAuth;
 use yii\rest\ActiveController;
 
 class PostController extends ActiveController
 {
     public $modelClass = Post::class;
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['authenticator']['only'] = ['create', 'update', 'delete'];
+
+        $behaviors['authenticator']['authMethods'] = [
+            HttpBearerAuth::class,
+        ];
+
+        return $behaviors;
+    }
 }
